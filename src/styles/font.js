700ec7159cc5d12a6cd98a34299a99c511dded1f
yerin-agreable <yerin@agreable.com>
2021-12-01 22:59:38 +0900

    feat: 스타일 목업 추가

---
+++ src/styles/font.js
@@ -0,0 +1,6 @@
+const font = {
+  default: 'Apple SD Gothic Neo, -apple-system, BlinkMacSystemFont, Malgun Gothic, sans-serif',
+  en: 'Montserrat, Apple SD Gothic Neo, -apple-system, BlinkMacSystemFont, Malgun Gothic, sans-serif'
+};
+
+export default font;
---
+++ src/styles/palette.js
@@ -0,0 +1,13 @@
+const palette = {
+  black: '#222222',
+  white: '#ffffff',
+  gray: '#666666',
+  gray_100: '#f8f8f8',
+  gray_200: '#f5f5f5',
+  gray_300: '#e1e1e1',
+  gray_400: '#cdcdcd',
+  gray_500: '#999999',
+  gray_600: '#666666',
+};
+
+export default palette;
