574cb59ec7070c7c0701ab52ed402d25ff63ec7d
Maetdol <rndka9@gmail.com>
2021-12-06 21:44:13 +0900

    feat: eslint 와 prettier 설정
    import 시 절대경로를 이용할 수 있게
    jsonconfig.json 파일을 추가했습니다

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,67 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    'react/function-component-definition': [
-      2,
-      {
-        namedComponents: 'function-declaration',
-      },
-    ],
-  },
+	env: {
+		browser: true,
+		es2021: true
+	},
+
+	extends: ['airbnb', 'prettier', 'plugin:react/recommended'],
+
+	parserOptions: {
+		ecmaFeature: {
+			jsx: true
+		},
+		ecmVersion: 12,
+		soureType: 'module'
+	},
+
+	plugins: ['react', 'react-hooks', 'import', 'prettier', 'jsx-a11y'],
+
+	rules: {
+		'react/function-component-definition': [
+			2,
+			{
+				namedComponents: 'function-declaration'
+			}
+		],
+
+		// 불필요한 공수를 줄이기 위해 js 대신 js를 허용
+		'react/jsx-filename-extension': 'off',
+
+		// 사용하지 않는 변수 에러에서 경고로 변경
+		'no-undef': 'warn',
+		'no-unused-vars': 'warn',
+		'react/jsx-uses-react': 'warn',
+		'react/jsx-uses-vars': 'warn',
+
+		// import React from 'react' 생략
+		'react/react-in-jsx-scope': 'off',
+
+		// { fnc: () => {} } 시 경고 표시,
+		// { fnc() {} } 형태 권장
+		'object-shorthand': 'warn',
+
+		// 한 줄당 최대 글자 수
+		'max-len': ['warn', { code: 100, ignoreComments: true }],
+
+		// if 문 {} 생략 가능
+		curly: 'off',
+
+		// 객체 속성 따옴표 쓰지 않게
+		'quote-props': ['error', 'as-needed'],
+
+		// trailing comma 해제
+		'comma-dangle': 'off',
+
+		// 비어있는 객체일 경우 중괄호 사이 줄넘김 금지
+		'object-curly-newline': ['warn', { multiline: true }],
+
+		// JSX 속성에 홑따옴표 사용 가능
+		'jsx-quotes': ['error', 'prefer-single'],
+
+		// PropTypes 사용하지 않게 변경
+		'react/prop-types': 'off',
+
+		// 스페이스 대신 탭 사용
+		indent: ['error', 'tab']
+	}
 };
---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+	// 줄넘김 lf로 고정
+	endOfLine: 'lf',
+
+	// 홑따옴표로 강제
+	singleQuote: true,
+	jsxSingleQuote: true,
+
+	// 오브젝트 속성에서 따옴표 필요할때만 사용하게
+	quoteProps: 'as-needed',
+
+	// 중괄호 전 후로 띄워쓰기
+	bracketSpacing: true,
+
+	// trailing comma disable
+	trailingComma: 'none',
+
+	// 한 줄당 최대 문자 수
+	printWidth: 100,
+
+	// 세미콜론 무조건 붙이게
+	semi: true,
+
+	// 탭 관련 설정
+	tabWidth: 2,
+	useTabs: true
+};
