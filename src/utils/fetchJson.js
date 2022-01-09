e994c0a84b39278deafe0d958590c12ff4d5d1b1
yerin-agreable <yerin@agreable.com>
2022-01-09 20:03:13 +0900

    feat: axios 설정 추가

---
+++ src/utils/fetchJson.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const FetchConsts = {
+  GET: "get",
+  POST: "post",
+  PUT: "put",
+  DELETE: "delete",
+};
+
+const axiosInstance = axios.create({timeout: 6000});
+
+const request = async (method, url, data = {}) => {
+  try {
+    const config = {url, method, data};
+    const result = await axiosInstance(config);
+    return result.data;
+  } catch (e) {
+    return console.log('@@ e', e);
+  }
+};
+
+export default {
+  get: (url, data) => request(FetchConsts.GET, url, data),
+  post: (url, data) => request(FetchConsts.POST, url, data),
+  put: (url, data) => request(FetchConsts.PUT, url, data),
+  delete: (url, data) => request(FetchConsts.DELETE, url, data),
+};
