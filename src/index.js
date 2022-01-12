5940f7c77c7cfd5fdfe985ed444cc095eea8595e
MaetDol <rndka9@gmail.com>
2022-01-12 22:06:27 +0900

    fix: ESLint 룰에 맞게 코드 변경

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 import { SWRConfig } from 'swr';
 import App from './App';
 
-const Root = () => {
-  return (
-    <SWRConfig
-      value={{
-        shouldRetryOnError: false,
-        revalidateOnFocus: false,
-        revalidateOnMount: true
-      }}
-    >
-      <App />
-    </SWRConfig>
-  );
-};
+const Root = () => (
+  <SWRConfig
+    value={{
+      shouldRetryOnError: false,
+      revalidateOnFocus: false,
+      revalidateOnMount: true
+    }}
+  >
+    <App />
+  </SWRConfig>
+);
 
 ReactDOM.render(<Root />, document.getElementById('root'));
